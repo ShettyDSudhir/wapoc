384d385346008a41440dbf34bfe477c8a2c85cd1
ShettyDSudhir <sudhirshetty@hotmail.com>
2023-01-19 16:11:36 +0530

    todays update

--- wabot.py
+++ wabot.py
@@ -1,49 +1,37 @@
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
-import json
+
+
 
 app = Flask(__name__)
 
-@app.route("/wa")
-def wa_hello():
-    return "Hello, World!"
+
 
 @app.route("/wasms", methods=['POST'])
 def wa_sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    Fetch_msg= request.form.to_dict()
-    #json_data = json.dumps(Fetch_msg, indent = 4) 
-    #print(json_data)
-    for key in Fetch_msg.keys():
-        print(key)
-        print("Value of key->",Fetch_msg[key])
 
+    wa_msg = request.form.to_dict()
+    
+    #print(wa_msg)
+    #checking if media exist needs to handle accordingly
 
-    try: # Storing the file that user send to the Twilio whatsapp number in our computer
-        msg_url=request.form.get('MediaUrl0')  # Getting the URL of the file
-        print("msg_url-->",msg_url)
-        msg_ext=request.form.get('MediaContentType0')  # Getting the extension for the file
-        print("msg_ext-->",msg_ext)
-        ext = msg_ext.split('/')[-1]
-        print("ext-->",ext)
-        if msg_url != None:
-            json_path = requests.get(msg_url)
-            filename = msg_url.split('/')[-1]
-            open(filename+"."+ext, 'wb').write(json_path.content)  # Storing the file
-    except:
-        print("no url-->>")
+    hasmedia = wa_msg['NumMedia']
+    if hasmedia == '1':
+        print("Media found")
+        print(wa_msg['MediaUrl0'])
+        print(wa_msg['MediaContentType0'])
 
-    msg = request.form.get('Body').lower()  # Reading the message from the whatsapp
-    print("msg-->",msg)
+    #lets extract the message and figure out how to handle it
+    msg = wa_msg['Body'].lower()  # Converting the message to lower case
     resp = MessagingResponse()
     reply=resp.message()
-    # Create reply
+
+    #call handler to handle the message
 
     # Text response
     if msg == "hi":
-       reply.body("hello! I am a bot")
+       reply.body("hello! I am just a bot")
 
     # Image response
     elif msg == "image":
@@ -61,9 +49,11 @@ def wa_sms_reply():
     # Document response
     elif msg == "file":
        reply.media('https://lienzo.s3.amazonaws.com/images/8d543af756864231e8bfa6532a230bd5-in-invoice-template-PDF-2.pdf')
+       reply.body("Your Invoice attached")
     
     else:
-        reply.body("to get order details send msg in the below format\n, Job id: xxxxxxxx")
+        strng = "Hi " + wa_msg['ProfileName'] + " you said '-" + wa_msg['Body'] + "-'"
+        reply.body(strng)
 
     return str(resp)
 
